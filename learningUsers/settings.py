45c91fd59a6f66f87878f905e4a8daf89019a696
Andrew Weinstein <andrew.weinstein@invitae.com>
2021-07-31 18:09:04 -0600

    registration and login/logout

--- learningUsers/settings.py
+++ learningUsers/settings.py
@@ -134,6 +134,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+LOGIN_URL = '/basic_app/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
